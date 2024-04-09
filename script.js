02de142625044f091347785ba452ba347b182f7d
AayushSrivastav29 <aayushsrivastav.13@gmail.com>
2024-04-09 19:36:33 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//variables
+const passwordDisplay = document.querySelector('[passwordDisplay]'); //custom-attribute
+const copyBtn = document.querySelector('[data-copy]');
+const copyMsg = document.querySelector('[data-copyMsg]');
+
+const lengthDisplay = document.querySelector('[data-lengthNmber]');
+const inputSlider = document.querySelector('.slider');
+
+const uppercaseCheck = document.querySelector('#uppercase');
+const lowercaseCheck = document.querySelector('#lowercase');
+const numbersCheck = document.querySelector('#numbers');
+const symbolsCheck = document.querySelector('#symbols');
+
+const indicator = document.querySelector('#light');
+const generateBtn = document.querySelector('.generate-button');
+const allCheckBox = document.querySelector('input[type=checkbox]');
